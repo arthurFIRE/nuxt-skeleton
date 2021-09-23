4bb6a131f1075492ba14683f4977ad02f2d2ad7d
김기만 <rlarlaks21c@naver.com>
2021-09-23 13:33:08 +0900

    first commit

---
+++ pages/test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Name/>
+    <Tutorial/>
+  </div>
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Name from '~/components/Name.vue'
+
+export default Vue.extend({components: {Name}})
+</script>
